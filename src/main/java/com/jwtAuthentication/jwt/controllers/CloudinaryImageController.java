1bf8363de9ceb86b96686e4bb15b01d586554596
Md-sarfraz <mdsarfraz62070@gmail.com>
2025-03-19 23:20:37 +0530

    working on pofile

--- src/main/java/com/jwtAuthentication/jwt/controllers/CloudinaryImageController.java
+++ src/main/java/com/jwtAuthentication/jwt/controllers/CloudinaryImageController.java
@@ -1,29 +1,29 @@
-//package com.jwtAuthentication.jwt.controllers;
-//
-//import com.jwtAuthentication.jwt.service.CloudinaryImageService;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
-//import org.springframework.web.bind.annotation.RestController;
-//import org.springframework.web.multipart.MultipartFile;
-//
-//import java.util.Map;
-//
-//@RestController
-//@RequestMapping("/cloudinary/upload")
-//public class CloudinaryImageController {
-//
-//    private final CloudinaryImageService cloudinaryImageService;
-//
-//    public CloudinaryImageController(CloudinaryImageService cloudinaryImageService) {
-//        this.cloudinaryImageService = cloudinaryImageService;
-//    }
-//
-//    @PostMapping
-//    public ResponseEntity<Map> uploadImg(@RequestParam("image") MultipartFile file){
-//        Map data=cloudinaryImageService.upload(file);
-//        return new ResponseEntity<>(data,HttpStatus.OK);
-//    }
-//}
+package com.jwtAuthentication.jwt.controllers;
+
+import com.jwtAuthentication.jwt.service.CloudinaryImageService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/cloudinary/upload")
+public class CloudinaryImageController {
+
+    private final CloudinaryImageService cloudinaryImageService;
+
+    public CloudinaryImageController(CloudinaryImageService cloudinaryImageService) {
+        this.cloudinaryImageService = cloudinaryImageService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Map> uploadImg(@RequestParam("image") MultipartFile file){
+        Map data=cloudinaryImageService.upload(file);
+        return new ResponseEntity<>(data,HttpStatus.OK);
+    }
+}
--- src/main/java/com/jwtAuthentication/jwt/service/CloudinaryImageService.java
+++ src/main/java/com/jwtAuthentication/jwt/service/CloudinaryImageService.java
@@ -1,11 +1,11 @@
-//package com.jwtAuthentication.jwt.service;
-//
-//import org.springframework.stereotype.Service;
-//import org.springframework.web.multipart.MultipartFile;
-//
-//import java.util.Map;
-//
-//@Service
-//public interface CloudinaryImageService {
-//    public Map upload(MultipartFile file);
-//}
+package com.jwtAuthentication.jwt.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Map;
+
+@Service
+public interface CloudinaryImageService {
+    public Map upload(MultipartFile file);
+}
